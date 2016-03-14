603fe5240aae11a9dbbb2a21b7ce7a919e728e75
jonnSmith <eugenpushkaroff@gmail.com>
2016-03-15 00:54:38 +0200

    vendorComposer

--- app/php/pdf.php
+++ app/php/pdf.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once '../php-vendor/dompdf/autoload.inc.php';
+error_reporting(E_ERROR);
+
+require_once '../php-vendor/autoload.php';
 
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
